1569f395c5a2e8ba862d640c915b7a0229a64807
Daniela Leal <falfada@gmail.com>
2024-08-02 16:42:48 -0500

    Created resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,31 @@
+import '../scss/pages/Resume.scss';
 export default function Resume(){
+    const fileUrl = 'src/assets/resume.pdf';
+    const fileName = 'resume.pdf';
     return(
-        <>Hello from Resume page</>
+        <section className="container resume">
+            <div className="resume-container">
+                <h2 className="resume-title">Skills</h2>
+                <ul className="resume-content">
+                    <li>UI/UX Design</li>
+                    <li>Responsive Design</li>
+                    <li>HTML/CSS</li>
+                    <li>JavaScript</li>
+                    <li>React</li>
+                    <li>Webpack</li>
+                    <li>Express</li>
+                    <li>Git, Github, Gitlab</li>
+                    <li>RESTful APIs, GraphQL</li>
+                    <li>MongoDB, PostgreSQL, MySQL</li>
+                    <li>Mongoose, Sequelize</li>
+                    <li>Jest</li>
+                    <li>Cross-functional team collaboration</li>
+                </ul>
+            </div>
+            <div className="resume-container">
+                <h2 className="resume-title">Resume</h2>
+                <a className="resume-content" href={fileUrl} download={fileName}>Download file 📥</a>
+            </div>
+        </section>
     )
 }
